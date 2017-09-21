3f2661ad0b093f24ffeb15cd00ac1dd6895c5c9a
Xavier Jorda <xavier.jorda@intrasonics.com>
2017-09-21 17:15:00 +0100

    Adding some extra documentation.

--- app/src/main/java/cat/jorda/tweetfilter/MainContract.java
+++ app/src/main/java/cat/jorda/tweetfilter/MainContract.java
@@ -1,7 +1,4 @@
 package cat.jorda.tweetfilter;
-
-import cat.jorda.tweetfilter.model.TweetItem;
-
 /**
  * Created by xj1 on 21/09/2017.
  *
@@ -11,14 +8,27 @@ interface MainContract
 {
     interface IMainView
     {
+        /**
+         * Callback call for when the adapter is being set.
+         * @param adapter adapter set up.
+         */
         void onAdapterSet(TweetsListAdapter adapter);
     }
 
     interface IMainPresenter
     {
+        /**
+         * Set a new arrays of key words that will be used for the Twitter API for the streaming filter.
+         * @param filterKeyWords String[] containing the keyworkds to be used by the filter().
+         */
         void setNewFilter(String[] filterKeyWords);
         void startTweetStreaming();
         void stopTweetStreaming();
+
+        /**
+         * Informs the presenter of the Touch events over the RecyclerView.
+         * @param isBeingTouched onTouchDown should be true and set it to false onTouchUp.
+         */
         void recyclerViewOnTouch(boolean isBeingTouched);
         void unregisterBroadcasts();
         void registerBroadcasts();
