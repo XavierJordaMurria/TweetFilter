c126d39fa3a37eb820b20a91cf004f1fb66b96c3
Xavier Jorda <xavier.jorda@intrasonics.com>
2017-10-10 10:52:21 +0100

    updating new icon

--- app/src/main/java/cat/jorda/tweetfilter/Constants.java
+++ app/src/main/java/cat/jorda/tweetfilter/Constants.java
@@ -7,8 +7,8 @@ package cat.jorda.tweetfilter;
 public class Constants
 {
     //Replace by your own key and secret from the Twietter developer account.
-    public static final String CONSUMER_KEY = "XXXXXXX";
-    public static final String CONSUMER_SECRET  = "XXXXXXX";
-    public static final String ACCESS_TOKEN  = "XXXXXXX-XXXXXXX";
-    public static final String ACCESS_TOKEN_SECRET  = "XXXXXXX";
+    public static final String CONSUMER_KEY = "jHlnViaDfBwj4dNpDptQIjUQ7";
+    public static final String CONSUMER_SECRET  = "D4uhvtKz8fCaLYz8PWv2RUysfQ7FYzFEeoVndBw9ULbUvHrI86";
+    public static final String ACCESS_TOKEN  = "910129791914913794-TkSzWwbqcF6T22pabnqYgPAgn6tpRgY";
+    public static final String ACCESS_TOKEN_SECRET  = "7GX1pTufvf2MOToWX6mftrv5TlqFORDAYBv5e3izOio1h";
 }
